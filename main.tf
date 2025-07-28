2a013e456b9cab4123b36facc3eea4c079a52120
vasim143 <vasim.tweet.349@gmail.com>
2025-07-28 20:55:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "myec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
+  #key_name      = var.key_name
+
   tags = {
     Name = "GitHubActions-EC2"
+    Environment = "dev"
   }
 }
