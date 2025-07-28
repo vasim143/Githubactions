e9eab8b6ba7a78b7df0d203c1e7d6a7773724804
vasim143 <vasim.tweet.349@gmail.com>
2025-07-28 18:15:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,5 +6,14 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
