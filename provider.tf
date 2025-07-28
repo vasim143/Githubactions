6ef1ee02a8a9433e56079957ce5afb571611d970
vasim143 <vasim.tweet.349@gmail.com>
2025-07-28 18:04:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
