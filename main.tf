ebf9302f19730c8bb703978bbd997ed9ed3526ac
vasim143 <vasim.tweet.349@gmail.com>
2025-07-28 18:20:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,7 @@
-provider "aws" {
-  region                  = "us-east-1"
-  access_key              = var.aws_access_key
-  secret_key              = var.aws_secret_key
-}
-
-resource "aws_instance" "githubactions_ec2" {
+resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  # key_name    = var.key_name   # REMOVE this line to avoid using key pair
+  key_name      = var.key_name
   tags = {
     Name = "GitHubActions-EC2"
   }
